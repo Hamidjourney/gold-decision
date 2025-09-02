76d0469a90fb73c345a030b12ba140531b275be6
Hamidjourney <datafortraffic@gmail.com>
2025-09-02 14:04:59 +0330

    Create update_goldhist.py

---
+++ scripts/update_goldhist.py
@@ -0,0 +1,4 @@
+# TODO: paste the daily updater here (pull Stooq, recompute indicators,
+# write data/Goldhist.csv and site/latest.json)
+if __name__ == "__main__":
+    print("update_goldhist.py placeholder")
